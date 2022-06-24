6ec5e0ef964b509f8cdbc45cf03a8a1b71d4ecc5
pablo <pablo.lucv@gmail.com>
2022-06-23 19:53:34 -0500

    Init project

---
+++ src/main/java/com/learnkaftka/KaftkaLibraryApplication.java
@@ -0,0 +1,13 @@
+package com.learnkaftka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaftkaLibraryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaftkaLibraryApplication.class, args);
+    }
+
+}
