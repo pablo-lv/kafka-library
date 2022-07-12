d29c9d4764534d978183af8f5e1d013c5ad30670
pablo <pablo.lucv@gmail.com>
2022-07-12 17:36:07 -0500

    changed to test embedded kafka

--- src/test/java/intg/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
+++ src/test/java/intg/com/learnkafka/controller/LibraryEventsControllerIntegrationTest.java
@@ -8,11 +8,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.*;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.TestPropertySource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest(classes = KafkaLibraryApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 //@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@EmbeddedKafka(topics = {"library-events"}, partitions = 3)
+@TestPropertySource(properties = {"spring.kafka.producer.bootstrap-servers=${spring.embedded.kafka.brokers}," +
+        "spring.kafka.admin.properties.bootstrap.servers=${spring.embedded.kafka.brokers}"})
 public class LibraryEventsControllerIntegrationTest {
 
     @Autowired
